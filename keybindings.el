c745d496476b85fb659bea99ef468be68e6758ca
Igor Kapkov <igasgeek@me.com>
2015-07-18 17:22:44 +0800

    Add neotree keybinding

---
+++ keybindings.el
@@ -0,0 +1,13 @@
+;;; keybindings.el --- igas Layer key-bindings File
+;;
+;; Copyright (c) 2015 Igor Kapkov
+;;
+;; Author: Igor Kapkov <igasgeek@me.com>
+;; URL: https://github.com/igas/igas-layer
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; NeoTree
+(define-key evil-normal-state-map (kbd "C-n") 'neotree-toggle)
